3f1c41688375e29715f2128065df79a99f5130ae
Stavros_Kanias <77228576+StavrosKanias@users.noreply.github.com>
2022-05-14 19:31:52 +0300

    Update compose.py

--- compose.py
+++ compose.py
@@ -8,16 +8,16 @@ def compose_video(edited_images, video_name, fps, total_frames, check_intervals,
     inputDir = "store_photos_out/"
     if mode == 'file':
         first_frame = cv2.imread(inputDir + "_frame_0.jpg")
-        image_list=os.listdir(inputDir)
-        edited_images=[]
+        image_list = os.listdir(inputDir)
+        edited_images = []
     for image_path in image_list:
-      input_path = inputDir+'/'+image_path
-      edited_images.append( cv2.imread(input_path))
+        input_path = inputDir+'/'+image_path
+        edited_images.append(cv2.imread(input_path))
     else:
         first_frame = edited_images[0]
     height, width, layers = first_frame.shape
-    is_dir = os.path.isdir('store_output_video/') 
-    if is_dir==False:
+    is_dir = os.path.isdir('store_output_video/')
+    if is_dir == False:
         os.makedirs('store_output_video/')
     videoWriter = cv2.VideoWriter(
         'store_output_video/{}.mp4'.format(video_name.split("/")[-1]), -1, fps, (width, height))
@@ -25,9 +25,8 @@ def compose_video(edited_images, video_name, fps, total_frames, check_intervals,
     for i in range(0, int(total_frames/check_intervals)-1):
         for j in range(0, check_intervals):
             videoWriter.write(edited_images[i])
-    if mode == 'file' and os.path.isfile(inputDir):
+    if mode == 'file' and os.path.isDir(inputDir):
         shutil.rmtree(inputDir)
 
     cv2.destroyAllWindows()
     videoWriter.release()
-
