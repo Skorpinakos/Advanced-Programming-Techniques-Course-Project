a8407ce61745ab36633d133f1defa0d36a2fc68a
Ioannis Tsampras <82767099+Skorpinakos@users.noreply.github.com>
2022-05-14 19:25:17 +0300

    Update olo mazi.py

--- olo mazi.py
+++ olo mazi.py
@@ -22,7 +22,7 @@ video_name = "directory/test.mp4" #choose input video location
 
 quality_factor=1
 
-
+file_mode='file'
 s1=time.time()
 #Open the video file
 cap = cv2.VideoCapture(video_name)
@@ -36,7 +36,7 @@ total_frames=cap.get(cv2.CAP_PROP_FRAME_COUNT)
 time_length =total_frames/fps
 #frame_list=[]
 
-images_after_split=split.split(frame_seq,total_frames,check_intervals,cap,mode='no_file')
+images_after_split=split.split(frame_seq,total_frames,check_intervals,cap,mode=file_mode)
 
 
 
@@ -63,7 +63,7 @@ ratio=width/height
 res=int(original_res*quality_factor)
 path = "store_photos_after_split"
 
-edited_images=detect.detect_and_edit(list_of_concerns,original_res,images_after_split,ratio,path,res,language='ell',mode='no_file')
+edited_images=detect.detect_and_edit(list_of_concerns,original_res,images_after_split,ratio,path,res,language='ell',mode=file_mode)
 
   
 #for i in range(0,len(frame_list)-1):                                         #<-it was about ram and ssd utilization
@@ -79,7 +79,7 @@ print(s3-s2)
 ######################THIRD_PHASE###################################
 
 
-compose.compose_video(edited_images,video_name,fps,total_frames,check_intervals,mode='no_file')
+compose.compose_video(edited_images,video_name,fps,total_frames,check_intervals,mode=file_mode)
 
 
 s4=time.time()
@@ -87,6 +87,8 @@ print("end of third phase")
 print(s4-s3)
 
 print("total ending",s4-s1)
+
+
 
 
 
