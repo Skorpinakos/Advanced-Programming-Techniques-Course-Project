d684c1c29091833c1c21a80785c00e9c51ebef64
Stavros_Kanias <77228576+StavrosKanias@users.noreply.github.com>
2022-05-15 16:02:33 +0300

    working example on port 8000

--- api_controller.py
+++ api_controller.py
@@ -5,17 +5,19 @@ from fastapi.responses import FileResponse
 import os
 import time
 
-list_of_concerns=[['ΙΩΑ',0.6],['ΙΩΑΝΝΗΣ',0.6],['ΤΣΑΜΠΡΑΣ',0.6],['ΣΤΑΜΑΤΙΟΥ',0.75],['ΙΩΑΝΝΗΣΤΣΑΜΠΡΑΣ',0.6],["up1066584",0.5]] #choose keywords and sensitivity
-video_name = "directory/test.mp4" #choose input video location
-quality_factor=1
-file_mode='file'
-check_intervals=30 #fps
+list_of_concerns = [['ΙΩΑ', 0.6], ['ΙΩΑΝΝΗΣ', 0.6], ['ΤΣΑΜΠΡΑΣ', 0.6], ['ΣΤΑΜΑΤΙΟΥ', 0.75], [
+    'ΙΩΑΝΝΗΣΤΣΑΜΠΡΑΣ', 0.6], ["up1066584", 0.5]]  # choose keywords and sensitivity
+video_name = "directory/test.mp4"  # choose input video location
+quality_factor = 1
+file_mode = 'file'
+check_intervals = 30  # fps
 
 
-#ocr_service.controller(list_of_concerns,video_name,quality_factor,file_mode,check_intervals)
+# ocr_service.controller(list_of_concerns,video_name,quality_factor,file_mode,check_intervals)
 
 
 app = FastAPI()
+
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
@@ -27,22 +29,23 @@ async def upload(file: UploadFile = File(...)):
             f.close()
         print('step1')
         try:
-            ocr_service.edit(list_of_concerns,'video_under_procces.mp4',quality_factor,file_mode,check_intervals)
+            ocr_service.edit(list_of_concerns, 'video_under_procces.mp4',
+                             quality_factor, file_mode, check_intervals)
         except Exception as error_name:
             print(error_name)
         print('step1')
         os.remove('video_under_procces.mp4')
         print('step1')
-        
+
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         await file.close()
-        
+
     time.sleep(1)
-    response_dict={'authors':'tsampras & kanias'}
-    return FileResponse('store_output_video/video_under_procces.mp4.mp4',headers=response_dict,media_type="video/mp4")
-    
+    response_dict = {'authors': 'tsampras & kanias'}
+    return FileResponse('store_output_video/video_under_procces.mp4.mp4', headers=response_dict, media_type="video/mp4")
+
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8001)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
--- videocensor.py
+++ videocensor.py
@@ -4,30 +4,37 @@ import shutil
 
 
 class Respond():
-  def __init__(self,resp):
-    self.info=resp.headers
-    self.video=resp.content
-class Censor():
-  def __init__(self,api_controller_path,storage_mode='RAM'):
-    self.api_controller_path=api_controller_path
-    self.process_to_execute=subprocess.Popen(["python", api_controller_path])
-    #self.process_to_execute.wait()
-  def terminate(self):
-    pass
-  def censor(self,file_path,keywords,options):
-    url = 'http://127.0.0.1:8001/upload'
-    file = {'file': open(file_path, 'rb')}
-    resp = requests.post(url=url, files=file) 
-    #print(resp.content)
-    #with open('edited_video.mp4','wb') as file:
-      #file.write(resp.content)
-      #file.close()
-    to_return=Respond(resp)
-    path_to_clear=self.api_controller_path.replace('api_controller.py','')+'store_output_video/'
-    shutil.rmtree(path_to_clear)
+    def __init__(self, resp):
+        self.info = resp.headers
+        self.video = resp.content
 
-    return  to_return
-    
-def initialize(location,storage_mode):
-  My_censor=Censor(location,storage_mode)
-  return My_censor
+
+class Censor():
+    def __init__(self, api_controller_path, storage_mode='RAM'):
+        self.api_controller_path = api_controller_path
+        self.process_to_execute = subprocess.Popen(
+            ["python", api_controller_path])
+        # self.process_to_execute.wait()
+
+    def terminate(self):
+        pass
+
+    def censor(self, file_path, keywords, options):
+        url = 'http://127.0.0.1:8000/upload'
+        file = {'file': open(file_path, 'rb')}
+        resp = requests.post(url=url, files=file)
+        # print(resp.content)
+        # with open('edited_video.mp4','wb') as file:
+        # file.write(resp.content)
+        # file.close()
+        to_return = Respond(resp)
+        path_to_clear = self.api_controller_path.replace(
+            'api_controller.py', '')+'store_output_video/'
+        shutil.rmtree(path_to_clear)
+
+        return to_return
+
+
+def initialize(location, storage_mode):
+    My_censor = Censor(location, storage_mode)
+    return My_censor
