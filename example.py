833d3288adfcb3d7be48c3a4e7446752271004ad
Ioannis Tsampras <82767099+Skorpinakos@users.noreply.github.com>
2022-05-17 22:06:44 +0300

    Update example.py

--- example.py
+++ example.py
@@ -6,8 +6,9 @@ location = 'api_controller.py'
 Censorer = videocensor.initialize(location, storage_mode='RAM')
 # use the tool on a local video
 result = Censorer.censor('directory/test.mp4',
-                         ['ΙΩΑΝΝΗΣ', 'ΤΣΑΜΠΡΑΣ'], {'quality_factor': 1, 'file_mode': 'file', 'check_intervals': 30})
+                         ['ΙΩΑΝΝΗΣ', 'ΤΣΑΜΠΡΑΣ'], {'quality_factor': 1, 'file_mode': 'no_file', 'check_intervals': 30})
 metadata = result.info  # get the metadata
 print(metadata)  # print return data
 with open('edited_video.mp4', 'wb') as file:
     file.write(result.video)  # save the returned video as file
+Censorer.kill()
