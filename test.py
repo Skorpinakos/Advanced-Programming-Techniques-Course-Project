d95761960c41630d8629f9433782f5df2792c3c1
Ioannis Tsampras <82767099+Skorpinakos@users.noreply.github.com>
2022-05-17 22:07:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import json
 
 # choose keywords and sensitivity
 params = {
-    'quality_factor': 1, 'file_mode': 'file', 'check_intervals': 30,
-    'keywords': ['sdfsd', 'sdfsfd']
+    'quality_factor': 1, 'file_mode': 'no_file', 'check_intervals': 30,
+    'keywords': ['ΙΩΆΝΝΗΣ', 'sdfsfd']
 }
 options = {'quality_factor': 1, 'file_mode': 'file', 'check_intervals': 30}
 file = {'file': open('directory/test.mp4', 'rb')}
@@ -15,3 +15,4 @@ resp = requests.post('http://127.0.0.1:8001/receive/',
 with open('edited_video.mp4', 'wb') as file:
     file.write(resp.content)
     file.close()
+
