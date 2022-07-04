17788a65d2f4ff77159a5286425954b2af8880e1
Ioannis Tsampras <82767099+Skorpinakos@users.noreply.github.com>
2022-07-04 19:48:13 +0300

    Update ocr_service.py

--- ocr_service.py
+++ ocr_service.py
@@ -2,6 +2,7 @@ import cv2
 import detect
 import compose
 import split
+import os
 
 
 def edit(list_of_concerns, video_name, quality_factor, file_mode, check_intervals):
@@ -23,6 +24,21 @@ def edit(list_of_concerns, video_name, quality_factor, file_mode, check_interval
         list_of_concerns, original_res, images_after_split, ratio, path, res, language='ell', mode=file_mode)
     compose.compose_video(edited_images, video_name, fps,
                           total_frames, check_intervals, mode=file_mode)
+
+def edit_photo(list_of_concerns, photo_name, quality_factor):
+    image=cv2.imread(photo_name)
+    original_res = height
+    ratio = width/height
+    height, width, layers = image.shape
+    res = int(original_res*quality_factor)
+    path="store_photos_after_split"
+    result=detect.detect_and_edit(list_of_concerns, original_res, [image], ratio, path, res, language='ell', mode='ram')
+    image=result[0]
+    if os.path.isdir('store_output_photo/') :
+        cv2.imwrite('store_output_photo/photo_under_procces.png', image)
+    else:
+        os.mkdir('store_output_photo/')
+        cv2.imwrite('store_output_photo/photo_under_procces.png', image)
 
 
 # controller(list_of_concerns,video_name,quality_factor,file_mode,check_intervals)
