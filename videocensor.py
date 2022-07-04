4dcfcf1acf3804c57773599b965fd658e239429e
Ioannis Tsampras <82767099+Skorpinakos@users.noreply.github.com>
2022-07-04 21:54:00 +0300

    Update videocensor.py

--- videocensor.py
+++ videocensor.py
@@ -7,14 +7,17 @@ import os
 class Respond():
     def __init__(self, resp):
         self.info = resp.headers
-        self.video = resp.content
-
-
+        self.media = resp.content
+class Photo():
+    def __init__(self, response,name):
+        self.info = response.info
+        self.media = response.media
+        self.name=name
 class Censor():
     def __init__(self, api_controller_path, storage_mode='RAM'):
+        self.url='http://127.0.0.1:8005/'
         self.api_controller_path = api_controller_path
-        self.process_to_execute = subprocess.Popen(
-            ["python", api_controller_path],creationflags = subprocess.CREATE_NO_WINDOW)
+        self.process_to_execute = subprocess.Popen(["python", api_controller_path],creationflags = subprocess.CREATE_NO_WINDOW)
         # self.process_to_execute.wait()
 
     def terminate(self):
@@ -24,17 +27,13 @@ class Censor():
 
 
     def censor(self, file_path, keywords, options):
-        url = 'http://127.0.0.1:8002/receive/'
+        url = self.url+'receive/'
         file = {'file': open(file_path, 'rb')}
-        #options.update({'keywords': keywords})
-        #print(options)
+
         params=options
         params['keywords']=keywords
         resp = requests.post(url,files=file, data=params)
-        # print(resp.content)
-        # with open('edited_video.mp4','wb') as file:
-        # file.write(resp.content)
-        # file.close()
+
         path_to_clear = self.api_controller_path.replace('api_controller.py', '')+'store_output_video/'
         shutil.rmtree(path_to_clear)
         os.remove(self.api_controller_path.replace('api_controller.py', '')+'video_under_procces.mp4')
@@ -43,37 +42,41 @@ class Censor():
 
     def censor_photos(self,user_path,keywords,options):
         if os.path.isdir(user_path):
+            
+            results=[]
+            
             for image in os.listdir(user_path):
-                results=[]
+                
                 if image.endswith((".png",".PNG")):
-                            url = 'http://127.0.0.1:8002/receive_photo/'
-                            file = {'file': open(image, 'rb')}
-                            #options.update({'keywords': keywords})
-                            #print(options)
+                            url = self.url+'receive_photo/'
+                            file = {'file': open(user_path+image, 'rb')}
+
                             params=options
+                            params['file_mode']='no_file'
+                            params['check_intervals']=1
                             params['keywords']=keywords
                             resp = requests.post(url,files=file, data=params)
-                            results.append(Respond(resp))
+                            results.append(Photo(Respond(resp),image))
+                            
             return results
         else:
-            url = 'http://127.0.0.1:8002/receive_photo/'
-            file = {'file': open(image, 'rb')}
-            #options.update({'keywords': keywords})
-            #print(options)
+            
+            url = self.url+'receive_photo/'
+            file = {'file': open(user_path, 'rb')}
+
             params=options
+            params['file_mode']='no_file'
+            params['check_intervals']=1
             params['keywords']=keywords
             resp = requests.post(url,files=file, data=params)
-            return Respond(resp)
+            path_to_clear = self.api_controller_path.replace('api_controller.py', '')+'store_output_photo/'
+            shutil.rmtree(path_to_clear)
+            os.remove(self.api_controller_path.replace('api_controller.py', '')+'photo_under_procces.png')
+            return Photo(Respond(resp),user_path.split('/')[-1])
 
 
 
 def initialize(location, storage_mode):
     My_censor = Censor(location, storage_mode)
     return My_censor
-
-
-#params = {
-   # 'quality_factor': 1, 'file_mode': 'no_file', 'check_intervals': 30,
-  #  'keywords': ['ΙΩΆΝΝΗΣ', 'sdfsfd']
-#}
 
